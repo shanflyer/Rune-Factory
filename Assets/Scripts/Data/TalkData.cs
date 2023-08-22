04bee8ac6920aa328a9bc48ec9b3be0056d66d23
shanflyer <shanflyer@qq.com>
2023-08-22 22:52:33 +0800

    Talk 00

---
+++ Assets/Scripts/Data/TalkData.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public enum TalkerDir
+{
+    左,右,无
+}
+[CreateAssetMenu(menuName ="Data/对话数据")]
+public class TalkData : ScriptableObject, IGameData
+{
+    public int id;
+    public bool myTalk;
+    public string talkerName;
+    public string talkerIconPath;
+    public Sprite talkerIcon;
+    public TalkerDir talkerDir;
+    public bool clearTalkIcon;
+    public string text;
+    public int nexTalkId;
+    public int actionId;
+    public string GetKey()
+    {
+        return id.ToString();
+    }
+    public override string ToString()
+    {
+        return id.ToString();
+    }
+
+    public void SetReferenceData()
+    {
+        talkerIcon = Resources.Load<Sprite>(talkerIconPath);
+    }
+}
---
+++ Assets/Scripts/UI/TalkPanel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TalkPanel : GamePanel
+{
+    TalkData talkData;
+
+  
+}
