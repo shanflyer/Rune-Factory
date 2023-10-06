9be8c3a0547fb2dad9cdc35d4f5c825943bcc408
shanflyer <shanflyer@qq.com>
2023-10-06 12:30:48 +0800

    打断延时任务

--- Assets/Scripts/Main/GameTimerController.cs
+++ Assets/Scripts/Main/GameTimerController.cs
@@ -1,31 +1,52 @@
 ﻿
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
 
 public class GameTimerController : Singleton<GameTimerController>
 {
-
+    Dictionary<Delegate, IEnumerator> waitIEnumerators = new Dictionary<Delegate, IEnumerator>();
+    Dictionary<Delegate, CancellationTokenSource> waitTasks = new Dictionary<Delegate, CancellationTokenSource>();
+    public override bool NeedUpdata => true;
     public override void Init()
     {
         base.Init();
     }
     public void DeleyActionMain(int delay, Action action)
     {
-        GameController.instance.StartCoroutine(Wait());
+        if (waitIEnumerators.TryGetValue(action,out var ienumerator))
+        {
+            GameController.instance.StopCoroutine(ienumerator);
+        }
+        IEnumerator IEnumerator = Wait();
+        waitIEnumerators[action] = ienumerator;
         IEnumerator Wait()
         {
             yield return new WaitForSeconds(delay*0.001f);
             action.Invoke();
+            waitIEnumerators.Remove(action);
         }
+        GameController.instance.StartCoroutine(IEnumerator);
     }
     public void DelayAction(int delay,Action action)
     {
+        if (waitTasks.TryGetValue(action, out var tokenSource))
+        {
+            tokenSource.Cancel();
+            tokenSource.Dispose();
+        }
+        var tokenSource2 = new CancellationTokenSource();
+        CancellationToken ct = tokenSource2.Token;
         Task task = Task.Factory.StartNew(async () =>
         {
             await Task.Delay(delay);
             action.Invoke();
-        });
+            waitTasks.Remove(action);
+            tokenSource2.Dispose();
+        }, tokenSource2.Token);
+        waitTasks[action] = tokenSource2;
     }
 }
