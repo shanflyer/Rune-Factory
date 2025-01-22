b563ef43877b1cfb16013c29b1d5fef2c7391514
shanflyer <shanflyer@qq.com>
2025-01-22 21:09:28 +0800

    云配置与调整

---
+++ Assets/Scripts/Cloud/CloudRemoteConfig.cs
@@ -0,0 +1,40 @@
+using System.Threading.Tasks;
+using Unity.Services.RemoteConfig;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using UnityEngine;
+public class CloudRemoteConfig : MonoBehaviour
+{
+    private async void Start()
+    {
+        if (Application.internetReachability!=NetworkReachability.NotReachable)
+        {
+            await InitializeRemoteConfigAsync();
+        }
+
+        RemoteConfigService.Instance.FetchCompleted += ApplyRemoteSettings;
+        RemoteConfigService.Instance.FetchConfigs(new userAttributes(), new appAttributes());
+    }
+ 
+    public struct userAttributes { }
+    public struct appAttributes { }
+
+    async Task InitializeRemoteConfigAsync()
+    {
+        // initialize handlers for unity game services
+        await UnityServices.InitializeAsync();
+
+        // remote config requires authentication for managing environment information
+        if (!AuthenticationService.Instance.IsSignedIn)
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        }
+    }
+
+ 
+
+    void ApplyRemoteSettings(ConfigResponse configResponse)
+    {
+        Debug.Log("RemoteConfigService.Instance.appConfig fetched: " + RemoteConfigService.Instance.appConfig.config.ToString());
+    }
+}
