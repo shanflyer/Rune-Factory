be672d9e8c7bdbce9f3a6cb55fa0f94e99ac5b6f
shanflyer <shanflyer@qq.com>
2026-05-23 13:30:57 +0800

    优化分帧加载

--- Assets/Scripts/Map/WorldMapController.cs
+++ Assets/Scripts/Map/WorldMapController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+using System;
+using System.Threading.Tasks;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -14,17 +15,22 @@ public class WorldMapController : MonoBehaviour
     public int runTimeHour { get => GameTimeManager.instance.Hour; set => GameTimeManager.instance.SetTime(value); }
 
     public int runTimeMinute { get => GameTimeManager.instance.Minute; set => GameTimeManager.instance.SetTime(minute: value); }
-
-
 #endif
+
     public static WorldMapController instance;
     WorldMapManager worldMapManager;
+
     [SerializeField]
     GameObject eventSystemObj;
+
     [SerializeField]
     string worldName;
+
     [SerializeField]
-    int characterId; 
+    int characterId;
+
+    private int initVersion;
+
     private void OnEnable()
     {
         instance = this;
@@ -33,85 +39,169 @@ public class WorldMapController : MonoBehaviour
         {
             eventSystemObj.SetActive(false);
         }
-        //Init();
+
         GameActionManager.instance.AddListener<StartWorldInit>(StartWorldInit);
         GameActionManager.instance.AddListener<LoadMapCompleted>(LoadMapCompleted);
     }
+
     void StartWorldInit(StartWorldInit startWorldInit)
     {
         Init();
     }
+
     void LoadMapCompleted(LoadMapCompleted loadMapCompleted)
     {
         GameDataSaveManager.instance.AfterInitMapLoadSaveData();
-        UIManager.instance.CloseGamePanel<LoadingPanel>(); 
+        UIManager.instance.CloseGamePanel<LoadingPanel>();
         WeatherManager.instance.RefreshWeather(GameTimeManager.instance.Hour);
     }
-    IEnumerator InitIEnumerator()
+
+    public async void Init()
     {
-        var loadGameSaveData= GameDataSaveManager.instance.loadGameSaveData;
+        int version = ++initVersion;
+        GameTimerController.instance.RemoveWaiter(AutoSave);
+
+        try
+        {
+            await EnsureCameraAsync();
+            if (!IsInitCurrent(version)) return;
+
+            await GameDataSaveManager.instance.InitLoadSaveData();
+            if (!IsInitCurrent(version)) return;
+
+            await InitWorldFlowAsync(version);
+        }
+        catch (Exception e)
+        {
+            if (IsInitCurrent(version))
+            {
+                Debug.LogException(e);
+            }
+        }
+    }
+
+    private async Task EnsureCameraAsync()
+    {
+        if (Camera.main != null) return;
+
+        var cameraPrefab = await GameSourceManager.instance.GetPrefab(DataPath.cameraPrefabPath);
+        if (cameraPrefab == null) return;
+
+        var async = InstantiateAsync(cameraPrefab);
+        await async;
+    }
+
+    private async Task InitWorldFlowAsync(int version)
+    {
+        _ = GameDataSaveManager.instance.loadGameSaveData;
         if (GameGuideManager.instance.endGuideFilmIndex > 0)
         {
             UIManager.instance.ShowGamePanel<LoadingPanel>();
         }
-        var teamManager = TeamManager.instance;
-        var shortcutManager = ShortcutManager.instance;
-        var environmentManger = EnvironmentManger.instance; 
-        yield return 0;
-        var npcManager = NPCManager.instance;
-        yield return 0;
-        var gameEventManager = GameEventManager.instance;
-        yield return 0;
-        var tempCharacterManager = TempCharacterManager.instance;
-        yield return 0;
-        var characterManager = CharacterManager.instance;
-        yield return 0;
-        var gameManager = GameManager.instance;
-        yield return 0;
-        var playerStoreManager = PlayerStoreManager.instance;
-        yield return 0;
-        var talkManager = TalkManager.instance;
-        yield return 0;
-        var farmManager = FarmManager.instance;
-        yield return 0;
-        var tempMapItemController = TempMapItemController.instance;
-        yield return 0;
-        var festivalManager = FestivalManager.instance;
-        yield return 0;
-        var gameTimeEventManager = GameTimeEventManager.instance;
-        yield return 0;
-        var gameVolumeMangaer = GameVolumeManager.instance;
-        yield return 0;
-        var timeLineManager = TimeLineManger.instance;
-        yield return 0;
-        var emote = EmoteManager.instance;
-        yield return 0;
-        var homeEquipManager = HomeEquipManager.instance;
-        yield return 0;
-        var manufatureManager = ManufactureManager.instance;
-        yield return 0;
-        var pastureManager = PastureManager.instance;
-        yield return 0;
-        var fisinghManager = FishingManager.instance;
-        yield return 0;
-        var fishController = FishController.instance;
-        yield return 0;
-        var weatherManager = WeatherManager.instance;
-        yield return 0;
+
+        _ = TeamManager.instance;
+        _ = ShortcutManager.instance;
+        _ = EnvironmentManger.instance;
+        if (!await YieldInitFrame(version)) return;
+
+        _ = NPCManager.instance;
+        if (!await YieldInitFrame(version)) return;
+
+        _ = GameEventManager.instance;
+        if (!await YieldInitFrame(version)) return;
+
+        _ = TempCharacterManager.instance;
+        if (!await YieldInitFrame(version)) return;
+
+        _ = CharacterManager.instance;
+        if (!await YieldInitFrame(version)) return;
+
+        _ = GameManager.instance;
+        if (!await YieldInitFrame(version)) return;
+
+        _ = PlayerStoreManager.instance;
+        if (!await YieldInitFrame(version)) return;
+
+        _ = TalkManager.instance;
+        if (!await YieldInitFrame(version)) return;
+
+        _ = FarmManager.instance;
+        if (!await YieldInitFrame(version)) return;
+
+        _ = TempMapItemController.instance;
+        if (!await YieldInitFrame(version)) return;
+
+        _ = FestivalManager.instance;
+        if (!await YieldInitFrame(version)) return;
+
+        _ = GameTimeEventManager.instance;
+        if (!await YieldInitFrame(version)) return;
+
+        _ = GameVolumeManager.instance;
+        if (!await YieldInitFrame(version)) return;
+
+        _ = TimeLineManger.instance;
+        if (!await YieldInitFrame(version)) return;
+
+        _ = EmoteManager.instance;
+        if (!await YieldInitFrame(version)) return;
+
+        _ = HomeEquipManager.instance;
+        if (!await YieldInitFrame(version)) return;
+
+        _ = ManufactureManager.instance;
+        if (!await YieldInitFrame(version)) return;
+
+        _ = PastureManager.instance;
+        if (!await YieldInitFrame(version)) return;
+
+        _ = FishingManager.instance;
+        if (!await YieldInitFrame(version)) return;
+
+        _ = FishController.instance;
+        if (!await YieldInitFrame(version)) return;
+
+        _ = WeatherManager.instance;
+        if (!await YieldInitFrame(version)) return;
+
         AudioController.instance.ClearBGM(AudioClearType.All, BGMGroup.Theme.ToString());
-       
-         yield return 0;
+        if (!await YieldInitFrame(version)) return;
+
         InputManager.instance.SwitchInputMap(false);
         GameActionManager.instance.QueueAction(new InitInputAction());
         GameTimeManager.instance.ZeroGameTime();
+        if (!await YieldInitFrame(version)) return;
 
-        yield return 0;
         GameDataSaveManager.instance.InitSaveDate();
+        if (!await YieldInitFrame(version)) return;
 
-        
-        yield return 0;
+        QueueInitialWorldActions();
+        if (!await YieldInitFrame(version)) return;
 
+        if (GameController.instance.startPlay)
+        {
+            NPCManager.instance.CreateZeroNPC();
+            GameActionManager.instance.QueueAction(new SwitchInputMap { UI = false });
+        }
 
+        if (!await YieldInitFrame(version)) return;
+        if (!await YieldInitFrame(version)) return;
+
+        GameTimeManager.instance.runTime = true;
+
+        if (!await YieldInitFrame(version)) return;
+        UIManager.instance.ShowGamePanel<MainPanel>();
+
+        if (!await YieldInitFrame(version)) return;
+        UIManager.instance.ShowGamePanel<ScreenControllerPanel>();
+        UIManager.instance.ShowGamePanel<PlayerTopPanel>();
+        UIManager.instance.ShowGamePanel<ShortcutPanel>();
+
+        ScheduleAutoSave();
+    }
+
+    private void QueueInitialWorldActions()
+    {
         if (GameGuideManager.instance.endGuideFilmIndex < 0 && !GameController.instance.noGuide)
         {
             GameActionManager.instance.QueueAction(new ChangeWorld
@@ -119,6 +209,7 @@ public class WorldMapController : MonoBehaviour
                 worldName = worldName,
                 displayMap = GameController.instance.ZeroMapInstance
             }, true);
+
             if (GameController.instance.startPlay)
             {
                 SetCharacterCoordinate setCharacterCoordinate = new SetCharacterCoordinate
@@ -129,7 +220,6 @@ public class WorldMapController : MonoBehaviour
                 };
                 GameActionManager.instance.QueueAction(setCharacterCoordinate);
             }
-           
         }
         else if (GameController.instance.noGuide || GameGuideManager.instance.endGuideFilmIndex >=
                  GameDataManager.instance.GlobalData.endGuideIndex)
@@ -139,6 +229,7 @@ public class WorldMapController : MonoBehaviour
                 worldName = worldName,
                 displayMap = GameController.instance.MapInstance
             }, true);
+
             SetCharacterCoordinate setCharacterCoordinate = new SetCharacterCoordinate
             {
                 characterId = characterId,
@@ -151,132 +242,34 @@ public class WorldMapController : MonoBehaviour
         {
             GameGuideManager.instance.SetGameGuidFilmDataAction(characterId, worldName);
         }
-       
+    }
 
-       
-        yield return 0;
-        if (GameController.instance.startPlay)
-        {
-            NPCManager.instance.CreateZeroNPC();
-            GameActionManager.instance.QueueAction(new SwitchInputMap { UI = false });
-        }
-        yield return 0;
-        
+    private async Task<bool> YieldInitFrame(int version)
+    {
+        await Task.Yield();
+        return IsInitCurrent(version);
+    }
 
-        yield return 0;
-       
-        //if (GameController.instance == null||GameController.instance.startPlay)
-        {
-            GameTimeManager.instance.runTime = true;
+    private bool IsInitCurrent(int version)
+    {
+        return this != null && isActiveAndEnabled && version == initVersion;
+    }
 
-            // if (GameController.instance.startPlay)
-            {
-                yield return 0;
-                UIManager.instance.ShowGamePanel<MainPanel>();
-                yield return 0;
-                UIManager.instance.ShowGamePanel<ScreenControllerPanel>();
-                UIManager.instance.ShowGamePanel<PlayerTopPanel>();
-                UIManager.instance.ShowGamePanel<ShortcutPanel>();
-            }
-            GameTimerController.instance.DelayAction(600000, AutoSave);
-
-        }
-
+    private void ScheduleAutoSave()
+    {
+        GameTimerController.instance.RemoveWaiter(AutoSave);
+        GameTimerController.instance.DelayAction(600000, AutoSave);
     }
 
     void AutoSave()
     {
         GameDataSaveManager.instance.TryAutoSaveData();
-      
-        GameTimerController.instance.DelayAction(600000, AutoSave);
+        ScheduleAutoSave();
     }
-    public async void Init()
-    {
-        if (Camera.main == null)
-        {
-            var cameraPrefab = await GameSourceManager.instance.GetPrefab(DataPath.cameraPrefabPath);
-            if (cameraPrefab != null)
-            {
-                var async = InstantiateAsync(cameraPrefab);
-                await async;
-                //Instantiate(cameraPrefab);
-            } 
-        }
-       
 
-        await GameDataSaveManager.instance.InitLoadSaveData();
-
-        GameController.instance.StartCoroutine(InitIEnumerator());
-        /*
-        var teamManager = TeamManager.instance;
-        var npcManager = NPCManager.instance;
-
-        var gameEventManager = GameEventManager.instance;
-        var tempCharacterManager = TempCharacterManager.instance;
-        var characterManager = CharacterManager.instance;
-        var gameManager = GameManager.instance;
-        var playerStoreManager = PlayerStoreManager.instance;
-        var talkManager = TalkManager.instance; 
-        var farmManager = FarmManager.instance;
-        var tempMapItemController = TempMapItemController.instance;
-        var festivalManager = FestivalManager.instance;
-        var gameTimeEventManager = GameTimeEventManager.instance;
-        var gameVolumeMangaer = GameVolumeManager.instance; 
-        var timeLineManager= TimeLineManger.instance;
-        var emote= EmoteManager.instance;
-        var homeEquipManager= HomeEquipManager.instance;
-        var manufatureManager = ManufatureManager.instance;
-        var pastureManager = PastureManager.instance;
-        var fisinghManager = FishingManager.instance;
-        var fishController = FishController.instance;
-        var weatherManager = WeatherManager.instance;
-
-        AudioController.instance.PlayBGM(null,audioClearType:AudioClearType.All, isLerp: true, Group: BGMGroup.Theme.ToString());
-
-        GameActionManager.instance.QueueAction(new ChangeWorld
-        {
-            worldName = worldName,
-            displayMap =GameController.instance.MapInstance
-        },true);
-        
-         
-        InputManager.instance.SwitchInputMap(false);
-        GameActionManager.instance.QueueAction(new InitInputAction());
-
-        var environmentManger = EnvironmentManger.instance;
-        GameTimeManager.instance.ZeroGameTime();
-
-
-        //if (GameController.instance == null||GameController.instance.startPlay)
-        {
-            GameTimeManager.instance.StartTimeRun();
-            SetCharacterCoordinate setCharacterCoordinate=new SetCharacterCoordinate 
-            { 
-                characterId = characterId,
-                coordinate =new int3(GameController.instance.Coordinate.xy, GameController.instance.MapInstance),  
-            };
-            GameActionManager.instance.QueueAction(setCharacterCoordinate);
-           
-            await UIManager.instance.ShowGamePanel<MainPanel>(); 
-           
-           await UIManager.instance.ShowGamePanel<ScreenControllerPanel>();
-        }
-       
-        if (GameController.instance.startPlay)
-        {
-            GameActionManager.instance.QueueAction(new SwitchInputMap { UI = false });
-        }
-
-        GameDataSaveManager.instance.AfterInitMapLoadSaveData();
-        GameDataSaveManager.instance.InitSaveDate();
-        WeatherManager.instance.RefreshWeather(GameTimeManager.instance.Hour);*/
-    }
-    // Use this for initialization
     void Start()
     {
-       
     }
-
 
 #if UNITY_EDITOR
     private void Update()
@@ -286,8 +279,9 @@ public class WorldMapController : MonoBehaviour
             SingletonType.instance.Update();
         }
     }
-#endif 
+#endif
 }
+
 #if UNITY_EDITOR
 [CustomEditor(typeof(WorldMapController))]
 public class WorldMapControllerEditor : Editor
@@ -296,9 +290,10 @@ public class WorldMapControllerEditor : Editor
     {
         get
         {
-           return target as WorldMapController;
+            return target as WorldMapController;
         }
     }
+
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
@@ -309,7 +304,6 @@ public class WorldMapControllerEditor : Editor
             gameController.runTimeHour = EditorGUILayout.IntSlider("Hour", gameController.runTimeHour, 0, 24);
             gameController.runTimeMinute = EditorGUILayout.IntSlider("Minute", gameController.runTimeMinute, 0, 60);
         }
-      
     }
 }
 #endif
